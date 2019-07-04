e28bc27f261ddb2bb328b9fd6f9f6bf52fc980b6
Tyler Sorensen <tyler@makerdao.com>
2019-07-04 16:02:58 +0200

    setup test file, can use pg-promise

---
+++ tests/govQueries.spec.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const pgp = require('pg-promise')();
+const cn = {
+    host: process.env.VL_DB_HOST,
+    port: process.env.VL_DB_PORT,
+    database: process.env.VL_DB_DATABASE,
+    user: process.env.VL_DB_USER,
+    password: process.env.VL_DB_PASSWORD
+};
+const db = pgp(cn);
+
+const insertLock = (t, values) => { //todo, import this from dsChiefTransformers
+  return t.none(
+    `
+INSERT INTO dschief.lock(contract_address, from_address, immediate_caller, lock, timestamp, log_index, tx_id, block_id) VALUES (\${contractAddress}, \${fromAddress}, \${immediateCaller}, \${lock}, \${timestamp}, \${logIndex}, \${txId}, \${blockId})`,
+    values,
+  );
+};
+
+test('can query db', async () => {
+  /*await insertLock(db,{
+      fromAddress: '0xfrom',
+      immediateCaller: '0xcaller',
+      lock: '3',
+      contractAddress: '0xaddress',
+      txId: 1,
+      blockId: 1,
+      logIndex: 3,
+      timestamp: '2017-11-25 18:24:17+00',
+    });*/
+  const l = await db.any('SELECT * FROM dschief.lock');
+  console.log('l', l);
+  expect(!!l).toBe(true);
+});
