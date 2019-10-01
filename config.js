6255c895430a08233a6f01a91f87635d0b7b7d3b
Tyler Sorensen <tyler@makerdao.com>
2019-10-01 19:51:01 +0100

    adding esm.joinMkr table

--- config.js
+++ config.js
@@ -6,18 +6,21 @@ const mkrTransformer = require("./transformers/MkrTransformer");
 const pollingTransformer = require("./transformers/PollingTransformer");
 const dsChiefTransformer = require("./transformers/DsChiefTransformer");
 const voteProxyFactoryTransformer = require("./transformers/VoteProxyFactoryTransformer");
+const esmTransformer = require("./transformers/EsmTransformer");
 
 //mainnet
 const MKR_ADDRESS = "0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2";
 const VOTING_CONTRACT_ADDRESS = "0xF9be8F0945acDdeeDaA64DFCA5Fe9629D0CF8E5D";
 const DSCHIEF_ADDRESS = "0x9eF05f7F6deB616fd37aC3c959a2dDD25A54E4F5";
 const VOTE_PROXY_FACTORY_ADDRESS = "0x868ba9aeacA5B73c7C27F3B01588bf4F1339F2bC";
+const ESM_ADDRESS = "0x5fd9cdc4c0ff1f121e3fd36b702cc419898e88f4";
 
 //kovan
 const MKR_KOVAN_ADDRESS = "0xaaf64bfcc32d0f15873a02163e7e500671a4ffcd";
 const VOTING_CONTRACT_KOVAN_ADDRESS = "0x518a0702701BF98b5242E73b2368ae07562BEEA3";
 const DSCHIEF_KOVAN_ADDRESS = "0xbbffc76e94b34f72d96d054b31f6424249c1337d";
 const VOTE_PROXY_FACTORY_KOVAN_ADDRESS = "0x3e08741a68c2d964d172793cd0ad14292f658cd8";
+const ESM_ADDRESS_KOVAN = "0x0f3582be7281022fca2ea5dc4c106983437d7de0"; //v0.2.14
 
 const kovan = {
   startingBlock: 5216304,
@@ -27,6 +30,7 @@ const kovan = {
       MKR_KOVAN_ADDRESS,
       DSCHIEF_KOVAN_ADDRESS,
       VOTE_PROXY_FACTORY_KOVAN_ADDRESS,
+      ESM_ADDRESS_KOVAN,
     ]),
   ],
   transformers: [
@@ -34,6 +38,7 @@ const kovan = {
     mkrTransformer(MKR_KOVAN_ADDRESS),
     dsChiefTransformer(DSCHIEF_KOVAN_ADDRESS),
     voteProxyFactoryTransformer(VOTE_PROXY_FACTORY_KOVAN_ADDRESS),
+    esmTransformer(ESM_ADDRESS_KOVAN)
   ],
   migrations: {
     mkr: "./migrations",
@@ -53,6 +58,7 @@ const mainnet = {
       MKR_ADDRESS,
       DSCHIEF_ADDRESS,
       VOTE_PROXY_FACTORY_ADDRESS,
+      ESM_ADDRESS,
     ]),
   ],
   transformers: [
@@ -60,6 +66,7 @@ const mainnet = {
     mkrTransformer(MKR_ADDRESS),
     dsChiefTransformer(DSCHIEF_ADDRESS),
     voteProxyFactoryTransformer(VOTE_PROXY_FACTORY_ADDRESS),
+    esmTransformer(ESM_ADDRESS)
   ],
   migrations: {
     mkr: "./migrations",
