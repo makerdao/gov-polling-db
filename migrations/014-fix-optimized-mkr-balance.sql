b2b619a6a4115ec2c3851eca6f2f681ee0f58d57
Tyler Sorensen <tyler@makerdao.com>
2019-10-25 17:58:27 +0900

    fix mkr.holders_on_block

---
+++ migrations/014-fix-optimized-mkr-balance.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE FUNCTION mkr.holders_on_block(arg_block_number INTEGER)
+RETURNS TABLE (
+address character varying(66),
+balance decimal(78,18)
+) AS $$
+SELECT SUMS.address, COALESCE(SUMS.sum, 0) + COALESCE(SUBS.sum, 0) as balance FROM (
+SELECT t.receiver as address, SUM(t.amount) FROM mkr.transfer_event t
+WHERE t.block_id <= (select max(id) from vulcan2x.block b where b.number <= arg_block_number)
+GROUP BY t.receiver
+) SUMS
+LEFT JOIN (
+SELECT sender as address, SUM(-t.amount) FROM mkr.transfer_event t 
+WHERE t.block_id <= (select max(id) from vulcan2x.block b where b.number <= arg_block_number)
+GROUP BY t.sender
+) SUBS ON (SUMS.address = SUBS.address);
+$$ LANGUAGE sql STABLE STRICT;
