1581f20ea705d00e037104601d7879f5f4cee29d
Lawrence Wang <l@lw.io>
2020-11-23 17:22:30 -0800

    account for mkr in the other wallet in a hot-cold pair

--- migrations/031-faster-polling-queries.sql
+++ migrations/031-faster-polling-queries.sql
@@ -9,9 +9,10 @@ returns decimal(78,18) as $$
 declare
   wallet_amount decimal(78,18);
   chief_amount decimal(78,18);
+  proxy%rowtype;
   proxy_wallet_amount decimal(78,18);
   proxy_chief_amount decimal(78,18);
-  proxy_address character(66);
+  linked_wallet_amount decimal(78,18);
 begin
   select amount into wallet_amount from mkr.balances ba
   where ba.address = weight_at_block.address
@@ -23,20 +24,27 @@ begin
   and ba.block_id <= weight_at_block.block_id
   order by ba.id desc limit 1;
 
-  select vote_proxy into proxy_address
+  select vote_proxy, hot into proxy_result
   from dschief.vote_proxy_created_event vpc
   where (hot = weight_at_block.address or cold = weight_at_block.address)
   and vpc.block_id <= weight_at_block.block_id
   order by vpc.id desc limit 1;
 
-  if proxy_address is not null then
+  if proxy is not null then
     select amount into proxy_wallet_amount from mkr.balances ba
-    where ba.address = proxy_address
+    where ba.address = proxy.vote_proxy
     and ba.block_id <= weight_at_block.block_id
     order by ba.id desc limit 1;
 
     select amount into proxy_chief_amount from dschief.balances ba
-    where ba.address = proxy_address
+    where ba.address = proxy.vote_proxy
+    and ba.block_id <= weight_at_block.block_id
+    order by ba.id desc limit 1;
+
+    select amount into linked_wallet_amount from mkr.balances ba
+    where ba.address = (
+      case when proxy.cold = weight_at_block.address
+      then proxy.hot else proxy.cold)
     and ba.block_id <= weight_at_block.block_id
     order by ba.id desc limit 1;
   end if;
@@ -44,7 +52,8 @@ begin
   return coalesce(wallet_amount, 0) + 
     coalesce(chief_amount, 0) + 
     coalesce(proxy_wallet_amount, 0) + 
-    coalesce(proxy_chief_amount, 0);
+    coalesce(proxy_chief_amount, 0) + ;
+    coalesce(linked_wallet_amount, 0);
 end;
 $$ language plpgsql stable strict;
 
@@ -71,5 +80,19 @@ returns table (
     option_id, 
     sum(amount)
   from polling.weighted_votes_at_block(poll_id, block_id)
+  group by option_id order by option_id asc;
+$$ language sql stable strict;
+
+create or replace function polling.tally_at_block_number(poll_id integer, block_number integer)
+returns table (
+  option_id integer,
+  amount decimal(78,18)
+) as $$
+  select
+    option_id, 
+    sum(amount)
+  from polling.weighted_votes_at_block(poll_id, (
+    select id from vulcan2x.block where number = block_number
+  ))
   group by option_id order by option_id asc;
 $$ language sql stable strict;
