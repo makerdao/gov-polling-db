b11baa84d7c80ebea267232e30543556ed45d627
Lawrence Wang <l@lw.io>
2020-11-24 11:55:40 -0800

    rename some functions

--- migrations/031-faster-polling-queries.sql
+++ migrations/031-faster-polling-queries.sql
@@ -4,7 +4,7 @@ create index receiver_index on mkr.transfer_event (receiver);
 create index block_id_index on mkr.transfer_event (block_id);
 create index timestamp_index on vulcan2x.block (timestamp);
 
-create or replace function polling.weight_at_block(address character(66), block_id integer)
+create or replace function polling.voter_weight(address character(66), block_id integer)
 returns decimal(78,18) as $$
 declare
   wallet_amount decimal(78,18);
@@ -15,37 +15,37 @@ declare
   linked_wallet_amount decimal(78,18);
 begin
   select amount into wallet_amount from mkr.balances ba
-  where ba.address = weight_at_block.address
-  and ba.block_id <= weight_at_block.block_id
+  where ba.address = voter_weight.address
+  and ba.block_id <= voter_weight.block_id
   order by ba.id desc limit 1;
 
   select amount into chief_amount from dschief.balances ba
-  where ba.address = weight_at_block.address
-  and ba.block_id <= weight_at_block.block_id
+  where ba.address = voter_weight.address
+  and ba.block_id <= voter_weight.block_id
   order by ba.id desc limit 1;
 
   select * into proxy
   from dschief.vote_proxy_created_event vpc
-  where (hot = weight_at_block.address or cold = weight_at_block.address)
-  and vpc.block_id <= weight_at_block.block_id
+  where (hot = voter_weight.address or cold = voter_weight.address)
+  and vpc.block_id <= voter_weight.block_id
   order by vpc.id desc limit 1;
 
   if proxy is not null then
     select amount into proxy_wallet_amount from mkr.balances ba
     where ba.address = proxy.vote_proxy
-    and ba.block_id <= weight_at_block.block_id
+    and ba.block_id <= voter_weight.block_id
     order by ba.id desc limit 1;
 
     select amount into proxy_chief_amount from dschief.balances ba
     where ba.address = proxy.vote_proxy
-    and ba.block_id <= weight_at_block.block_id
+    and ba.block_id <= voter_weight.block_id
     order by ba.id desc limit 1;
 
     select amount into linked_wallet_amount from mkr.balances ba
     where ba.address = (
-      case when proxy.cold = weight_at_block.address
+      case when proxy.cold = voter_weight.address
       then proxy.hot else proxy.cold end)
-    and ba.block_id <= weight_at_block.block_id
+    and ba.block_id <= voter_weight.block_id
     order by ba.id desc limit 1;
   end if;
 
@@ -57,7 +57,7 @@ begin
 end;
 $$ language plpgsql stable strict;
 
-create or replace function polling.weighted_votes_at_block(poll_id integer, block_id integer)
+create or replace function polling.votes(poll_id integer, block_id integer)
 returns table (
   voter character(66),
   option_id integer,
@@ -66,33 +66,30 @@ returns table (
   select 
     voter, 
     option_id, 
-    polling.weight_at_block(voter, weighted_votes_at_block.block_id)
-  from polling.valid_votes(weighted_votes_at_block.poll_id) vv
-  where vv.block_id <= weighted_votes_at_block.block_id
+    polling.weight_at_block(voter, votes.block_id)
+  from polling.valid_votes(votes.poll_id) vv
+  where vv.block_id <= votes.block_id
 $$ language sql stable strict;
 
-create or replace function polling.tally_at_block(poll_id integer, block_id integer)
+create or replace function polling.votes_at_block(poll_id integer, block_number integer)
 returns table (
+  voter character(66),
   option_id integer,
   amount decimal(78,18)
 ) as $$
-  select
-    option_id, 
-    sum(amount)
-  from polling.weighted_votes_at_block(poll_id, block_id)
-  group by option_id order by option_id asc;
-$$ language sql stable strict;
-
-create or replace function polling.tally_at_block_number(poll_id integer, block_number integer)
-returns table (
-  option_id integer,
-  amount decimal(78,18)
-) as $$
-  select
-    option_id, 
-    sum(amount)
-  from polling.weighted_votes_at_block(poll_id, (
+  select * from polling.votes(poll_id, (
     select id from vulcan2x.block where number = block_number
-  ))
-  group by option_id order by option_id asc;
+  )) 
+$$ language sql stable strict;
+
+create or replace function polling.votes_at_time(poll_id integer, unixtime integer)
+returns table (
+  voter character(66),
+  option_id integer,
+  amount decimal(78,18)
+) as $$
+  select * from polling.votes(poll_id, (
+    select id from vulcan2x.block where extract(epoch from timestamp) <= unixtime
+    order by id desc limit 1
+  )) 
 $$ language sql stable strict;
