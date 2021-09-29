9abe4de040da4ef705a4333172ffb0fd0e491fac
Adam Goth <adamgoth@gmail.com>
2021-09-29 15:34:40 +0200

    Add goerli config

--- config.js
+++ config.js
@@ -34,6 +34,51 @@ const DSCHIEF_12_KOVAN_ADDRESS = '0x27E0c9567729Ea6e3241DE74B3dE499b7ddd3fe6';
 const VOTE_PROXY_FACTORY_12_KOVAN_ADDRESS = "0x1400798AA746457E467A1eb9b3F3f72C25314429";
 const VOTE_DELEGATE_FACTORY_KOVAN_ADDRESS = "0x1740F3bD55b1900C816A0071F8972C201566e3a3";
 
+//goerli
+// note: VOTING_CONTRACT_KOVAN_ADDRESS and SECOND_VOTING_CONTRACT_KOVAN_ADDRESS are the same contract on goerli
+// note: there is no v1 of DSCHIEF or VOTE_PROXY_FACTORY deployed to goerli, only the newer versions
+const MKR_GOERLI_ADDRESS = "0xc5E4eaB513A7CD12b2335e8a0D57273e13D499f7";
+const BATCH_VOTING_CONTRACT_GOERLI_ADDRESS = "0xdbE5d00b2D8C13a77Fb03Ee50C87317dbC1B15fb";
+const ESM_ADDRESS_GOERLI = "0x105BF37e7D81917b6fEACd6171335B4838e53D5e";
+const DSCHIEF_12_GOERLI_ADDRESS = '0x33Ed584fc655b08b2bca45E1C5b5f07c98053bC1';
+const VOTE_PROXY_FACTORY_12_GOERLI_ADDRESS = "0x1a7c1ee5eE2A3B67778ff1eA8c719A3fA1b02b6f";
+const VOTE_DELEGATE_FACTORY_GOERLI_ADDRESS = "0xE2d249AE3c156b132C40D07bd4d34e73c1712947";
+
+const goerli = {
+  startingBlock: 5273000,
+  extractors: [
+    ...makeRawLogExtractors([
+      BATCH_VOTING_CONTRACT_GOERLI_ADDRESS,
+      MKR_GOERLI_ADDRESS,
+      ESM_ADDRESS_GOERLI,
+      DSCHIEF_12_GOERLI_ADDRESS,
+      VOTE_PROXY_FACTORY_12_GOERLI_ADDRESS,
+      VOTE_DELEGATE_FACTORY_GOERLI_ADDRESS,
+    ]),
+  ],
+  transformers: [
+    pollingTransformer(BATCH_VOTING_CONTRACT_GOERLI_ADDRESS),
+    mkrTransformer(MKR_GOERLI_ADDRESS),
+    mkrBalanceTransformer(MKR_GOERLI_ADDRESS),
+    esmTransformer(ESM_ADDRESS_GOERLI),
+    dsChiefTransformer(DSCHIEF_12_GOERLI_ADDRESS, '_v1.2'),
+    chiefBalanceTransformer(DSCHIEF_12_GOERLI_ADDRESS, '_v1.2'),
+    voteProxyFactoryTransformer(VOTE_PROXY_FACTORY_12_GOERLI_ADDRESS, '_v1.2'),
+    voteDelegateFactoryTransformer(VOTE_DELEGATE_FACTORY_GOERLI_ADDRESS),
+  ],
+  migrations: {
+    mkr: "./migrations",
+  },
+  api: {
+    whitelisting: {
+      enabled: false,
+    },
+    responseCaching: {
+      enabled: false,
+      duration: "15 seconds",
+    },
+  },
+}
 
 const kovan = {
   startingBlock: 5216304,
@@ -125,9 +170,12 @@ let config;
 if (process.env.VL_CHAIN_NAME === "mainnet") {
   console.log("Using mainnet config");
   config = mainnet;
-} else {
+} else if (process.env.VL_CHAIN_NAME === "kovan") {
   console.log("Using kovan config");
   config = kovan;
+} else {
+  console.log("Using goerli config");
+  config = goerli;
 }
 
 module.exports.default = config;
