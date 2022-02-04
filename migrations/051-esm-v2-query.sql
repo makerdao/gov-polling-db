cc8666a16c3698c0c108deb31ff0d2db7930889a
Adam Goth <adamgoth@gmail.com>
2022-02-04 15:49:22 +0100

    Add new esm info

---
+++ migrations/051-esm-v2-query.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE FUNCTION api.all_esm_v2_joins()
+RETURNS TABLE (
+  tx_from character varying(66),
+  tx_hash character varying(66),
+  join_amount decimal(78,18),
+  block_timestamp TIMESTAMP WITH TIME ZONE
+) AS $$
+SELECT j.from_address, t.hash, j.join_amount, b.timestamp
+FROM esmV2.mkr_joins j
+LEFT JOIN vulcan2x.transaction t
+ON j.tx_id = t.id
+LEFT JOIN vulcan2x.block b
+ON j.block_id = b.id;
+$$ LANGUAGE sql STABLE STRICT;
---
+++ transformers/EsmV2Transformer.js
@@ -0,0 +1,40 @@
+const { getExtractorName } = require("spock-etl/lib/core/processors/extractors/instances/rawEventDataExtractor");
+const { handleEvents } = require("spock-etl/lib/core/processors/transformers/common");
+// @ts-ignore
+const ESMAbi = require("../abis/esm_v2_abi.json");
+const { getTxByIdOrDie } = require("spock-etl/lib/core/processors/extractors/common");
+const BigNumber = require("bignumber.js").BigNumber;
+
+module.exports = address => ({
+  name: "ESMTransformer",
+  dependencies: [getExtractorName(address)],
+  transform: async (services, logs) => {
+    await handleEvents(services, ESMAbi, logs[0], handlers);
+  },
+});
+
+const handlers = {
+  Join: async (services, { event, log }) => {
+    const tx = await getTxByIdOrDie(services, log.tx_id);
+
+    await insertJoin(services, {
+      fromAddress: tx.from_address,
+      immediateCaller: event.params.usr,
+      joinAmount: new BigNumber(event.params.wad)
+        .div(new BigNumber("1e18"))
+        .toString(),
+      contractAddress: event.address,
+      txId: log.tx_id,
+      blockId: log.block_id,
+      logIndex: log.log_index
+    });
+  },
+};
+
+const insertJoin = (s, values) => {
+  return s.tx.none(
+    `
+INSERT INTO esmV2.mkr_joins(contract_address, from_address, immediate_caller, join_amount, log_index, tx_id, block_id) VALUES (\${contractAddress}, \${fromAddress}, \${immediateCaller}, \${joinAmount}, \${logIndex}, \${txId}, \${blockId})`,
+    values,
+  );
+};
