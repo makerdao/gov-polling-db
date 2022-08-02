31316b0f39f5bdc72a8e1715a0a28f706494c7f4
Tyler Sorensen <tyler@dux.makerdao.network>
2022-08-01 23:11:27 -0700

    update comments syntax

--- migrations/056-manual-lock-fix-2.sql
+++ migrations/056-manual-lock-fix-2.sql
@@ -1,7 +1,7 @@
-### Temporary fix since the UI uses the tx.origin of lock events to group delegation events
-### Gnosis Safe address is 0xf65475e74C1Ed6d004d5240b06E3088724dFDA5d
-### Main address used is 0xc0583df0d10c2e87ae1873b728a0bda04d8b660c
-### But also used this address twice: 0x2487fb0baa85e550fda42396d8c99ab03bd23fe1 <- this is the address being overwritten here
+--Temporary fix since the UI uses the tx.origin of lock events to group delegation events
+--Gnosis Safe address is 0xf65475e74C1Ed6d004d5240b06E3088724dFDA5d
+--Main address used is 0xc0583df0d10c2e87ae1873b728a0bda04d8b660c
+--But user also used this address twice, which we are overwriting now: 0x2487fb0baa85e550fda42396d8c99ab03bd23fe1
 
 UPDATE dschief.lock l
 SET from_address = '0xc0583df0d10c2e87ae1873b728a0bda04d8b660c'
