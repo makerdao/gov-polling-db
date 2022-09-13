1ccc946e6297b759cf0367bbe21b9e8e148f4689
Tyler Sorensen <tyler@dux.makerdao.network>
2022-09-13 23:18:38 +0200

    add back logIndex, make contract address lowercase, improve readability

--- transformers/DsChiefTransformer.js
+++ transformers/DsChiefTransformer.js
@@ -28,30 +28,35 @@ const handlers = {
   "free(uint256)": async (services, { log, note }) => {
     const tx = await getTxByIdOrDie(services, log.tx_id);
 
-    
+    //get delegate event
     try {
+      //TODO: use RetryProvider
       const provider = ethers.getDefaultProvider(process.env.VL_CHAIN_HOST);
       const transaction = await provider.getTransaction(tx.hash);
+      //get transaction receipt
       const { logs } = await transaction.wait();
-      const logInfo = logs.find(l => l.topics[0] === FreeTopic);
-      if (logInfo) {
+      //get event(s) from Delegate contract. Usually only one per transaction
+      const delegateLogArray = logs.filter(l => l.topics[0] === FreeTopic);
+      delegateLogArray.forEach(async delegateLog => {
         await insertDelegateLock(services, {
           fromAddress: tx.from_address,
-          immediateCaller: '0x' + logInfo.topics[1].slice(-40),
-          lock: new BigNumber(logInfo.data).div(new BigNumber("1e18")).negated().toString(),
-          contractAddress: logInfo.address,
+          immediateCaller: '0x' + delegateLog.topics[1].slice(-40),
+          lock: new BigNumber(delegateLog.data).div(new BigNumber("1e18")).negated().toString(),
+          contractAddress: delegateLog.address.toLowerCase(),
           txId: log.tx_id,
           blockId: log.block_id,
+          logIndex: delegateLog.logIndex,
         });
-      }
+      });
     } catch (e) {
       console.log('error trying to find delegate free event', e);
     }
 
-    //don't add if already exists in DB
     const row = await services.db.oneOrNone(vdQuery, [log.tx_id, log.log_index]);
-    console.log('skipping chief.lock event since it\'s already in the DB', row);
-    if (row) return;
+    if (row) {
+      console.log('skipping chief.lock event since it\'s already in the DB', row);
+      return;
+    }
 
     await insertLock(services, {
       fromAddress: tx.from_address,
@@ -69,35 +74,41 @@ const handlers = {
   "lock(uint256)": async (services, { log, note }) => {
     const tx = await getTxByIdOrDie(services, log.tx_id);
 
+    //get delegate event
     try {
+      //TODO: use RetryProvider
       const provider = ethers.getDefaultProvider(process.env.VL_CHAIN_HOST);
       const transaction = await provider.getTransaction(tx.hash);
+      //get transaction receipt
       const { logs } = await transaction.wait();
-      const logInfo = logs.find(l => l.topics[0] === LockTopic);
-      if (logInfo) {
+      //get event(s) from Delegate contract. Usually only one per transaction
+      const delegateLogArray = logs.filter(l => l.topics[0] === LockTopic);
+      delegateLogArray.forEach(async delegateLog => {
         await insertDelegateLock(services, {
           fromAddress: tx.from_address,
-          immediateCaller: '0x' + logInfo.topics[1].slice(-40),
-          lock: new BigNumber(logInfo.data).div(new BigNumber("1e18")).toString(),
-          contractAddress: logInfo.address,
+          immediateCaller: '0x' + delegateLog.topics[1].slice(-40),
+          lock: new BigNumber(delegateLog.data).div(new BigNumber("1e18")).toString(),
+          contractAddress: delegateLog.address,
           txId: log.tx_id,
           blockId: log.block_id,
+          logIndex: delegateLog.logIndex,
         });
-      }
+      });
     } catch (e) {
       console.log('error trying to find delegate lock event', e);
     }
 
-    //don't add if already exists in DB
     const row = await services.db.oneOrNone(vdQuery, [log.tx_id, log.log_index]);
-    console.log('skipping chief.lock event since it\'s already in the DB', row);
-    if (row) return;
+    if (row) {
+      console.log('skipping chief.lock event since it\'s already in the DB', row);
+      return;
+    }
 
     await insertLock(services, {
       fromAddress: tx.from_address,
       immediateCaller: note.caller,
       lock: new BigNumber(note.params.wad).div(new BigNumber("1e18")).toString(),
-      contractAddress: log.address,
+      contractAddress: log.address.toLowerCase(),
       txId: log.tx_id,
       blockId: log.block_id,
       logIndex: log.log_index,
@@ -116,7 +127,7 @@ INSERT INTO dschief.lock(contract_address, from_address, immediate_caller, lock,
 const insertDelegateLock = (s, values) => {
   return s.tx.none(
     `
-INSERT INTO dschief.delegate_lock(contract_address, from_address, immediate_caller, lock, tx_id, block_id) VALUES (\${contractAddress}, \${fromAddress}, \${immediateCaller}, \${lock}, \${txId}, \${blockId})`,
+INSERT INTO dschief.delegate_lock(contract_address, from_address, immediate_caller, lock, log_index, tx_id, block_id) VALUES (\${contractAddress}, \${fromAddress}, \${immediateCaller}, \${lock}, \${logIndex}, \${txId}, \${blockId})`,
     values,
   );
 };
