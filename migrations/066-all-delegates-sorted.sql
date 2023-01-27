d580879fe9ac729473df9be925f8de006858e6ff
hernandoagf <hernando.guzman93@gmail.com>
2023-01-27 19:09:40 -0300

    Add more columns, refactor function

--- migrations/066-all-delegates-sorted.sql
+++ migrations/066-all-delegates-sorted.sql
@@ -1,86 +1,60 @@
 create type order_direction_type as enum ('ASC', 'DESC');
 create type order_by_type as enum ('DATE', 'MKR', 'DELEGATORS');
+create type delegate_entry as (
+  delegate character varying(66),
+  vote_delegate character varying(66),
+  creation_date timestamp with time zone,
+  expiration_date timestamp with time zone,
+  expired boolean,
+  delegator_count int,
+  total_mkr numeric(78,18)
+);
 
--- This function will be called dynamically by api.delegates() based on the user sorting preference
-create or replace function dschief.delegates_by_date(include_expired boolean, order_direction order_direction_type default 'ASC')
-returns setof dschief.vote_delegate_created_event as $$
-with A as (
-  select dels.*
-  from dschief.vote_delegate_created_event dels
-  left join vulcan2x.block blocks
-  on dels.block_id = blocks.id
-  where
-    -- Check that the difference between the current date and the delegate creation date is lower than a year (31536000 seconds)
-    (case when include_expired then true else extract(epoch from now()) - extract(epoch from blocks.timestamp) < 31536000 end)
-)
-select A.*
-from A
-order by 
-  case when order_direction = 'ASC' then A.block_id else -A.block_id end asc;
+-- Small function used to return the max page size for paginated endpoints
+create or replace function api.max_page_size()
+returns int as $$
+select 30
 $$ language sql stable strict;
 
--- This function will be called dynamically by api.delegates() based on the user sorting preference
-create or replace function dschief.delegates_by_mkr(include_expired boolean, order_direction order_direction_type default 'DESC')
-returns setof dschief.vote_delegate_created_event as $$
-with A as (
-  select dels.*
-  from dschief.vote_delegate_created_event dels
-  left join vulcan2x.block blocks
-  on dels.block_id = blocks.id
-  where
-    -- Check that the difference between the current date and the delegate creation date is lower than a year (31536000 seconds)
-    (case when include_expired then true else extract(epoch from now()) - extract(epoch from blocks.timestamp) < 31536000 end)
-)
-select A.*
-from A
-left join dschief.delegate_lock B
-on A.vote_delegate = B.contract_address
-group by A.vote_delegate, A.id, A.delegate, A.log_index, A.tx_id, A.block_id
-order by 
-  case when order_direction = 'ASC' then sum(coalesce(B.lock, 0)) else -sum(coalesce(B.lock, 0)) end asc;
-$$ language sql stable strict;
-
--- This function will be called dynamically by api.delegates() based on the user sorting preference
-create or replace function dschief.delegates_by_delegators(include_expired boolean, order_direction order_direction_type default 'DESC')
-returns setof dschief.vote_delegate_created_event as $$
-with A as (
-  select dels.*
-  from dschief.vote_delegate_created_event dels
-  left join vulcan2x.block blocks
-  on dels.block_id = blocks.id
-  where
-    -- Check that the difference between the current date and the delegate creation date is lower than a year (31536000 seconds)
-    (case when include_expired then true else extract(epoch from now()) - extract(epoch from blocks.timestamp) < 31536000 end)
-)
-select A.*
-from A
-left join (
-  select contract_address, count(immediate_caller) as delegators
-  from (select immediate_caller, sum(lock) as delegations, contract_address
-  from dschief.delegate_lock
-  group by contract_address, immediate_caller) as D
-  where delegations > 0
-  group by contract_address
-) B
-on A.vote_delegate = B.contract_address
-group by A.vote_delegate, A.id, A.delegate, A.log_index, A.tx_id, A.block_id, B.delegators
-order by 
-  case when order_direction = 'ASC' then coalesce(delegators, 0) else -coalesce(delegators, 0) end asc;
-$$ language sql stable strict;
-
--- Function exposed to the API, it dynamically calls one of the other three based on the user input
 create or replace function api.delegates(_first int, order_by order_by_type default 'DATE', order_direction order_direction_type default 'DESC', include_expired boolean default false)
-returns setof dschief.vote_delegate_created_event as $$
+returns setof delegate_entry as $$
+declare
+  max_page_size_value int := (select api.max_page_size());
 begin
-  if _first > 30 or _first < 1 then
-    raise exception 'Parameter first only accepts a number between 1 and 30.';
+  if _first > max_page_size_value then
+    raise exception 'Parameter FIRST cannot be greater than %.', max_page_size_value
     return;
-  elsif order_by = 'MKR' then
-    return query select * from dschief.delegates_by_mkr(include_expired, order_direction);
-  elsif order_by = 'DELEGATORS' then
-    return query select * from dschief.delegates_by_delegators(include_expired, order_direction);
   else
-    return query select * from dschief.delegates_by_date(include_expired, order_direction);    
+    return query
+      with delegates_table as (
+        select A.delegate, A.vote_delegate, B.timestamp as creation_date, B.timestamp + '1 year' as expiration_date, now() > B.timestamp + '1 year' as expired
+        from dschief.vote_delegate_created_event A
+        left join vulcan2x.block B
+        on A.block_id = B.id
+        -- Filter out expired delegates if include_expired is false
+        where include_expired or now() < B.timestamp + '1 year'
+      ), delegations_table as (
+        select contract_address, count(immediate_caller) as delegators, sum(delegations) as delegations
+          from (
+            select immediate_caller, sum(lock) as delegations, contract_address
+            from dschief.delegate_lock
+            group by contract_address, immediate_caller
+          ) as D
+          where delegations > 0
+          group by contract_address
+      )
+      select delegates_table.*, coalesce(delegators, 0)::int as delegator_count, coalesce(delegations, 0)::numeric(78,18) as total_mkr
+      from delegates_table
+      left join delegations_table
+      on delegates_table.vote_delegate = delegations_table.contract_address
+      order by case
+        when order_by = 'DELEGATORS' then
+          case when order_direction = 'ASC' then coalesce(delegators, 0)::int else -coalesce(delegators, 0)::int end
+        when order_by = 'MKR' then
+          case when order_direction = 'ASC' then coalesce(delegations, 0)::numeric(78,18) else -coalesce(delegations, 0)::numeric(78,18) end
+        else
+          case when order_direction = 'ASC' then extract(epoch from creation_date) else -extract(epoch from creation_date) end
+        end;
   end if;
 end;
 $$ language plpgsql stable strict;
