c7b65fa47190bcb5684191b09c674e183849b337
tyler17 <tylerya@gmail.com>
2023-09-07 15:15:42 -0700

    fix script

--- insert_poll_creators.js
+++ insert_poll_creators.js
@@ -1,30 +1,34 @@
 require('dotenv').config();
 
 const pgp = require('pg-promise')();
-const authorizedCreatorsArray = (process.env.AUTHORIZED_CREATORS || '').split(',').map(creator => creator.toLowerCase());
 
-const cn = {
+const db = pgp({
   host: process.env.VL_DB_HOST,
   port: process.env.VL_DB_PORT,
   database: process.env.VL_DB_DATABASE,
   user: process.env.VL_DB_USER,
   password: process.env.VL_DB_PASSWORD
-};
+});
 
-const db = pgp(cn);
+const authorizedCreatorsArray = (process.env.AUTHORIZED_CREATORS || '').split(',').map(creator => creator.toLowerCase());
 
 const deleteQuery = 'DELETE FROM polling.creators';
 const insertQuery = 'INSERT INTO polling.creators (address) VALUES ($1)';
 
-function executeQueries(connection) {
-  return connection.none(deleteQuery)
-    .then(() => connection.tx(t => t.batch(authorizedCreatorsArray.map(creator => connection.none(insertQuery, creator)))))
-    .catch(err => console.error('Error message: ', err.message, 'Stack trace: ', err.stack));
+async function executeQueries() {
+  try {
+    await db.none(deleteQuery);
+    console.log('All previous creators deleted.');
+
+    for (const creator of authorizedCreatorsArray) {
+      await db.none(insertQuery, creator);
+      console.log(`Creator ${creator} added successfully.`);
+    }
+
+    console.log('All authorized creators added successfully.');
+  } catch (err) {
+    console.error('Error: ', err);
+  }
 }
 
-db.connect()
-  .then(connection => {
-    executeQueries(connection)
-      .finally(() => connection.done());
-  })
-  .catch(err => console.error('Error message: ', err.message, 'Stack trace: ', err.stack));
+executeQueries().catch(err => console.error('Error: ', err));
